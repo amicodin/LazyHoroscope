fa7c2014d76865567cc41746094ab1c54e4b92bf
Anthony G Micodin <amicodin@ecc-jaguar.csuchico.edu>
2018-09-19 16:13:07 -0700

    Added the variables and user inputs

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Anthony Micodin
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,16 @@ using namespace std;
 int main()
 {
   //declare variables
+  int lucky;
+  string first;
+  string last;
 
   //get user input
+  cout<<"What is your first name?"<<endl;
+  cin>>first;
+
+  cout<<"What is your last name?"<<endl;
+  cin>>last;
 
   //tell fortune
 

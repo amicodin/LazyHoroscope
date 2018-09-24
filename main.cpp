8dadd5db7eba21ea09151934230b4c89566a379d
Anthony G Micodin <amicodin@o244-20.csuchico.edu>
2018-09-24 16:40:46 -0700

    finished part 2 of lab

--- main.cpp
+++ main.cpp
@@ -18,31 +18,31 @@ int main()
   cout<<"What is your last name?"<<endl;
   cin>>last;
 
-  cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune ..."<<endl;
+  cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"., here is your fortune..."<<endl;
 
   //tell fortune
   lucky = first.length();
-  cout<<"Your lucky number is "<<lucky<<endl;
+  cout<<"your lucky number is "<<lucky<<endl;
   
   
   if (first[0] == 'a'|| first[0] == 'e' || first[0] == 'i' || first[0] == 'o'|| first[0] == 'u'|| first[0] == 'A' || first[0] == 'E' || first[0] == 'I' || first[0] == 'O' || first[0] == 'U' )
   {
-   cout<<"You are destined to be famous"<<endl;
+   cout<<"you are destined to be famous!"<<endl;
   }       
 
   else 
   {
-   cout<<"You should keep a low profile"<<endl;
+   cout<<"you should keep a low profile."<<endl;
   }
   
   int index = last.length() - 1;
 
  if (last[index] == 'a' || last[index] == 'A' || last[index] == 'e' || last[index] == 'E' || last[index] == 'i' || last[index] == 'I' || last[index] == 'o' || last[index] == 'O' || last[index] == 'u' || last[index] == 'U')
  {
-         cout<<"You have already met your true love."<<endl;
+         cout<<"you have already met your true love."<<endl;
  }
 
- cout<<"Have a good day!"<<endl;
+ cout<<"have a good day!"<<endl;
   return 0;
 }
 

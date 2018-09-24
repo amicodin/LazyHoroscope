2d0e250d2b75abcd6504912f2355791fc9d490f4
Anthony G Micodin <amicodin@o244-20.csuchico.edu>
2018-09-24 16:33:52 -0700

    finished part 2 of lab

--- main.cpp
+++ main.cpp
@@ -23,5 +23,26 @@ int main()
   //tell fortune
   lucky = first.length();
   cout<<"Your lucky number is "<<lucky<<endl;
+  
+  
+  if (first[0] == 'a'|| first[0] == 'e' || first[0] == 'i' || first[0] == 'o'|| first[0] == 'u'|| first[0] == 'A' || first[0] == 'E' || first[0] == 'I' || first[0] == 'O' || first[0] == 'U' )
+  {
+   cout<<"You are destined to be famous"<<endl;
+  }       
+
+  else 
+  {
+   cout<<"You should keep a low profile"<<endl;
+  }
+  
+  int index = last.length() - 1;
+
+ if (last[index] == 'a' || last[index] == 'A' || last[index] == 'e' || last[index] == 'E' || last[index] == 'i' || last[index] == 'I' || last[index] == 'o' || last[index] == 'O' || last[index] == 'u' || last[index] == 'U')
+ {
+         cout<<"You have already met your true love."<<endl;
+ }
+
+ cout<<"Have a good day!"<<endl;
   return 0;
 }
+
